e2010e2498f5c3701300cef42f35928e0ae71b69
nadiyasabria <2400018236@webmail.uad.ac.id>
2025-05-27 16:25:06 +0700

    Add files via upload

---
+++ [236][RESPONSI UTS ALPRO][SELASA13.45][D].cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string username, barang;
+	int password;
+	cout<<"============ LOGIN ============ "<<endl;
+	cout<<" username: ";
+	cin>>username;
+	cout<<" password: ";
+	cin>>password;
+	
+	cout<<"daftar barang yang tersedia: "<<endl;
+	while(barang){
+		break;
+		cout<<"1. roti - 12.000"<<endl;
+		cout<<"2. galon isi ulang - 7.000"<<endl;
+		cout<<"3. sabun - 24.000"<<endl;
+		cout<<"4. parfum - 47.000"<<endl;
+		default;
+	}
+	return 1;
+}
+{
+
+	cout<<"============ DAFTAR BELANJA ============ "<<endl;
+	cout<<"masukkan daftar barang belanjaan: "<<endl;
+	cin>>barang;
+	cout<<"apakah anda ingin menghapus barang ini? (y/n)"<<endl;
+	cin>>hapusbarang;
+	(y==1 || n==0)
+	cout<<"total belanja anda adalah: "<<endl;
+	totalpembelian=barang*harga
+	
+	if {totalpembelian>100.000==15%
+}else{totalpembelian<100.000==0%
+}
+	cout<<"banyaknya pelanggan supermarket yang telah belanja: "<<endl;
+	cin>>banyakpelanggan;
+	cout<<"total pembelian pelanggan supermarket: "<<endl;
+	cin>>totalpembelian;
+	
+
+return 0;
+
+}
